cffe7195d498fba9acd32601a6b8e6c9d5577fe8
farchettiensis <farchettiensis@users.noreply.github.com>
2023-06-21 23:45:28 -0300

    Part 3.11-15

--- part03-11_second_and_second_to_last/src/second_and_second_to_last.py
+++ part03-11_second_and_second_to_last/src/second_and_second_to_last.py
@@ -1,1 +1,9 @@
 # Write your solution here
+string = input("Please type in a string: ")
+second = string[1]
+second_to_last = string[-2]
+
+if len(string) > 1 and second == second_to_last:
+    print(f"The second and the second to last characters are {second}")
+else:
+    print("The second and the second to last characters are different")
--- part03-15_right_aligned/src/right_aligned.py
+++ part03-15_right_aligned/src/right_aligned.py
@@ -1,1 +1,5 @@
 # Write your solution here
+string = input("Please type in a string: ")
+stars = 20 - len(string)
+
+print("*" * stars + string)
