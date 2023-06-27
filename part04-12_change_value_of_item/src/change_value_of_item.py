890594b4534ad502f32626f07b4f31cd1bf83d0e
farchettiensis <farchettiensis@users.noreply.github.com>
2023-06-26 23:54:46 -0300

    A few more from part 4

--- part04-12_change_value_of_item/src/change_value_of_item.py
+++ part04-12_change_value_of_item/src/change_value_of_item.py
@@ -1,1 +1,12 @@
 # Write your solution here
+list = [1, 2, 3, 4, 5]
+
+while True:
+    index = int(input("Index: "))
+    if index == -1:
+        break
+    if index < 0 or index >= len(list):
+        print("Invalid index")
+        continue
+    list[index] = int(input("New value: "))
+    print(list)
--- part04-13_add_items_to_list/src/add_items_to_list.py
+++ part04-13_add_items_to_list/src/add_items_to_list.py
@@ -1,1 +1,10 @@
 # Write your solution here
+i = int(input("How many items: "))
+j = 1
+list = []
+
+while i >= j:
+    new_item = int(input(f"Item {j}: "))
+    list.append(new_item)
+    j += 1
+print(list)
