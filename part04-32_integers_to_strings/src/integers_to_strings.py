3ded72d23cc44c02eaf9fa439d323d58505ce0e1
farchettiensis <farchettiensis@users.noreply.github.com>
2023-07-01 23:49:58 -0300

    Part 4.32-33

--- part04-32_integers_to_strings/src/integers_to_strings.py
+++ part04-32_integers_to_strings/src/integers_to_strings.py
@@ -1,1 +1,10 @@
 # Write your solution here
+def formatted(float_list: list):
+    formatted_strings = []
+    for i in float_list:
+        formatted_strings.append(f"{i:.2f}")
+    return formatted_strings
+
+if __name__ == "__main__":
+    print(formatted([1.234567, 2.345678, 3.456789]))
+
--- part04-33_everything_reversed/src/everything_reversed.py
+++ part04-33_everything_reversed/src/everything_reversed.py
@@ -1,1 +1,10 @@
 # Write your solution here
+def everything_reversed(str_list: list):
+    reversed_list = []
+    for word in str_list:
+        reversed_list.append(word[::-1])
+    return reversed_list[::-1]
+
+if __name__ == "__main__":
+    print(everything_reversed(["Ingen", "ko", "på", "isen"]))
+
