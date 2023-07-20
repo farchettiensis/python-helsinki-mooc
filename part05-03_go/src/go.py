6fc7f4d19ff12ae2b119c0497cc3fedf13523ee2
farchettiensis <farchettiensis@users.noreply.github.com>
2023-07-20 00:46:44 -0300

    A few more from part 05

--- part05-03_go/src/go.py
+++ part05-03_go/src/go.py
@@ -1,1 +1,22 @@
 # Write your solution here
+def who_won(game_board: list):
+    player1_pieces = 0
+    player2_pieces = 0
+
+    for row in game_board:
+        for cell in row:
+            if cell == 1:
+                player1_pieces += 1
+            elif cell == 2:
+                player2_pieces += 1
+
+    if player1_pieces > player2_pieces:
+        return 1
+    elif player2_pieces > player1_pieces:
+        return 2
+    else:
+        return 0
+
+if __name__ == "__main__":
+    m = [[1, 2, 1], [0, 3, 4], [1, 0, 0]]
+    print(who_won(m))
--- part05-04_sudoku_row/src/sudoku_row.py
+++ part05-04_sudoku_row/src/sudoku_row.py
@@ -1,1 +1,35 @@
 # Write your solution here
+def row_correct(sudoku: list, row_no: int):
+    row = sudoku[row_no]
+    size = len(row)
+    for i in range(1, size + 1):
+        if row.count(i) > 1:
+            return False
+    return True
+
+if __name__ == "__main__":
+    sudoku = [
+        [9, 0, 0, 0, 8, 0, 3, 0, 0],
+        [2, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 1, 0, 0, 1, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    ]
+    print(row_correct(sudoku, 0))
+    print(row_correct(sudoku, 1))
+    print(row_correct(sudoku, 6))
+
+    """ Model solution
+    def row_correct(sudoku: list, row_no: int):
+    numbers = []
+    for number in sudoku[row_no]:
+        if number > 0 and number in numbers:
+            return False
+        numbers.append(number)
+ 
+    return True
+    """
